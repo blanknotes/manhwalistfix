64dcdfd64fc241800ffe71d1dca53fd26c86f6db
blanknotes <120952406+blanknotes@users.noreply.github.com>
2026-02-28 15:24:35 +0700

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,5 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/quick-list-creator.ts';
-import '@/ai/flows/manhwa-recommendation.ts';
--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-3857372427-5de0c",
-  "appId": "1:332294682658:web:1101efbfae46b30b80327b",
-  "apiKey": "AIzaSyCkpjtvMHH8EriQoDRj_YnrW2QR2c5-2Uw",
-  "authDomain": "studio-3857372427-5de0c.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "332294682658"
-};
